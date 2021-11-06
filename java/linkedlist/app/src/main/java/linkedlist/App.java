3d07df39e133d6fa182fdbb5c2bb4a3b3dccdccc
Kafaween <abod.kafaween@gmail.com>
2021-11-06 18:36:59 +0200

    done

--- java/linkedlist/app/src/main/java/linkedlist/App.java
+++ java/linkedlist/app/src/main/java/linkedlist/App.java
@@ -7,36 +7,53 @@ public class App {
 
     public static void main(String[] args) {
 
-      System.out.println("---------------------------------");
-      linkedlistStructure list =new linkedlistStructure();
-      System.out.println(list.to_string());
-      System.out.println("--------------------------");
-      list.insert("3");
-      list.insert("5");
-      list.insert("1");
-      list.insert("9");
-      System.out.println(list.to_string());
-      System.out.println( list.includes("3"));
-      list.append("10");
-      System.out.println(list.to_string());
-      list.insert_before("5","15");
-      System.out.println(list.to_string());
-      list.insert_after("5","16");
-      System.out.println(list.to_string());
+//      System.out.println("---------------------------------");
+//      linkedlistStructure list =new linkedlistStructure();
+//      System.out.println(list.to_string());
+//      System.out.println("--------------------------");
+//      list.insert("3");
+//      list.insert("5");
+//      list.insert("1");
+//      list.insert("9");
+//      System.out.println(list.to_string());
+//      System.out.println( list.includes("3"));
+//      list.append("10");
+//      System.out.println(list.to_string());
+//      list.insert_before("5","15");
+//      System.out.println(list.to_string());
+//      list.insert_after("5","16");
+//      System.out.println(list.to_string());
+//
+//      list.linked_list_kth(6);
+//      System.out.println(list.linked_list_kth(2));
+//      System.out.println("-------------------------------------------------------------------------");
+//      linkedlistStructure list2 =new linkedlistStructure();
+//      list2.insert("8");
+//      list2.insert("5");
+//      list2.insert("6");
+//      list2.insert("2");
+//      System.out.println(list.to_string());
+//      System.out.println(list2.to_string());
+//
+//      list.linked_list_zip(list,list2);
+//      System.out.println("after zipping");
+////      System.out.println(list.to_string());
+//      Test test= new Test();
+//
+//      test.insert("3");
+//      test.insert("1");
+      Test test2 =new Test();
+      test2.insert("a");
+      test2.insert("b");
+      test2.insert("c");
+      test2.insert("c");
+      test2.insert("b");
+      test2.insert("a");
+      System.out.println(test2.to_string());
 
-      list.linked_list_kth(6);
-      System.out.println(list.linked_list_kth(2));
-      System.out.println("-------------------------------------------------------------------------");
-      linkedlistStructure list2 =new linkedlistStructure();
-      list2.insert("8");
-      list2.insert("5");
-      list2.insert("6");
-      list2.insert("2");
-      System.out.println(list.to_string());
-      System.out.println(list2.to_string());
+      System.out.println(test2.palindrom());
 
-      list.linked_list_zip(list,list2);
-      System.out.println("after zipping");
-      System.out.println(list.to_string());
+
+
     }
 }
---
+++ java/linkedlist/app/src/main/java/linkedlist/Test.java
@@ -0,0 +1,189 @@
+package linkedlist;
+
+import java.util.Stack;
+
+public class Test {
+  private   linkedlistNode head;
+  private int size;
+
+  public boolean palindrom(){
+    linkedlistNode current= head;
+    Stack stack= new Stack();
+    while (current!=null){
+      stack.push(current.getValue());
+      current=(linkedlistNode) current.getNext();
+    }
+    current=head;
+    while (current!=null&&!stack.isEmpty()){
+      if(!(current.getValue().equals(stack.pop()))){
+        return false;
+      }
+      current=(linkedlistNode) current.getNext();
+    }
+    return true;
+  }
+
+  public void Reverse(){
+    linkedlistNode current=head;
+    linkedlistNode prev= null;
+    linkedlistNode next = null;
+
+    while (current!=null){
+      next=(linkedlistNode) current.getNext();
+      current.setNext(prev);
+      prev=current;
+      current=next;
+    }
+    head=prev;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+  public linkedlistNode Zip(Test l1,Test l2){
+    if(l1.size==0&&l2.size==0)return null;
+    if(l1.size==0)return l2.head;
+    if(l2.size==0)return l1.head;
+    linkedlistNode current1=l1.head;
+    linkedlistNode current2=l2.head;
+    boolean smaller=false;
+    while (current1!=null&&current2!=null){
+     linkedlistNode tmp=(linkedlistNode) current1.getNext();
+      linkedlistNode tmp2=(linkedlistNode) current2;
+      current2= (linkedlistNode) current2.getNext();
+     current1.setNext(tmp2);
+      current1.getNext().setNext(tmp);
+      if(tmp==null){
+        smaller=true;
+        break;
+      }
+      current1=(linkedlistNode) current1.getNext().getNext();
+    }
+    if(smaller==true){
+      current1.getNext().setNext(current2);
+    }
+    return l1.head;
+  }
+
+
+
+
+
+
+
+
+  public void InsertAfter(String value,String newV){
+    if(head==null){
+      System.out.println("list is empty");
+    }
+    else if(value.equals(head.getValue())){
+      linkedlistNode node= new linkedlistNode(newV);
+      linkedlistNode tmp=(linkedlistNode) head.getNext();
+      head.setNext(node);
+      node.setNext(tmp);
+      size++;
+      return;
+    }
+    linkedlistNode current=head;
+    linkedlistNode node= new linkedlistNode(newV);
+    boolean Notfound=true;
+    while(current!=null&&Notfound){
+      if(value.equals(current.getValue())){
+        linkedlistNode tmp=(linkedlistNode) current.getNext();
+        current.setNext(node);
+        node.setNext(tmp);
+        size++;
+        Notfound=false;
+      }
+      current=(linkedlistNode) current.getNext();
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  public void insert(String value){
+    if(head ==null){
+      linkedlistNode node=new linkedlistNode(value);
+      head=node;
+      size++;
+    }
+    else{
+      linkedlistNode node =new linkedlistNode((value));
+      node.setNext(head);
+      head=node;
+      size++;
+    }
+  }
+
+  public boolean includes(String value){
+    if(is_empty()) {
+      System.out.println("\n The list is empty ");
+      return false;
+    }
+    else {
+      System.out.println("Is "+value+"in the kist ???");
+      linkedlistNode current;
+      current=head;
+      while (current!=null){
+        if(current.getValue()==value){
+          return true;
+        }
+        else{
+          current= (linkedlistNode) current.getNext();
+        }
+      }
+    }
+    return false;
+  }
+
+  public String to_string() {
+    if (head == null) {
+      return "List is empty";
+    } else {
+      linkedlistNode current;
+      current = head;
+      String output="";
+
+
+
+      while (current != null) {
+        // moves the current reference along the list
+        output=output+("{ "+current+" }" + " -> ");
+        current =(linkedlistNode) current.getNext();
+      }
+
+      return (output+"NULL");
+    }
+  }
+  public int size() {
+    return size;
+  }
+  public boolean is_empty(){
+    if (size==0){
+      return true ;
+    }
+    else{
+      return false;
+    }
+  }
+}
